86dab7d9bcfbf76daed676a0a4246c2c962a3dc6
ipla2 <ipla2@student.monash.edu>
2018-08-03 12:37:25 +1000

    created project src structure

--- settings.py
+++ settings.py
@@ -9,19 +9,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# example) MySQL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': '',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '',
-#     }
-# }
-
 INSTALLED_APPS = (
     'data',
     'django_extensions'
