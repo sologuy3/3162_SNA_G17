021e34809ad86298f30e3bf445e96befe0628eaf
Yohan Francis <yohan@keaz.co>
2018-08-10 13:48:18 +1000

    skeleton for unittest

---
+++ src/3162_UnitTests/testNode.py
@@ -0,0 +1,15 @@
+import unittest
+from graph import node
+
+class NodeTest(unittest.TestCase):
+    def setUp(self):
+        """Call before every test case."""
+
+    def tearDown(self):
+        """Call after every test case."""
+
+    def testA(self):
+        """Test case A. note that all test method names must begin with 'test.'"""
+
+if __name__ == "__main__":
+    unittest.main() # run all tests
