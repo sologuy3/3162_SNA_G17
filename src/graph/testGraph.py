55a41af1319a967893bf974a8d96b339caa8075c
ipla2 <ipla2@student.monash.edu>
2018-08-17 16:38:50 +1000

    tests for graph

--- src/graph/testGraph.py
+++ src/graph/testGraph.py
@@ -8,7 +8,6 @@ class GraphTest(unittest.TestCase):
         """Call before every test case."""
         self.graph_a = Graph(name="myGraph")
 
-
         # Simple graph with three nodes
         node1 = Node()
         node2 = Node()
@@ -18,40 +17,5 @@ class GraphTest(unittest.TestCase):
         self.graph_a.add_nodes(node2, neighbors={node1})
         self.graph_a.add_nodes(node3, neighbors={node1})
 
-
-
     def tearDown(self):
         """Call after every test case."""
-
-
-    # def test_add_neighbor_at_init(self):
-    #     """Unit test for passing a list of neighbors as an argument when initializing a node'"""
-    #     neighbor = Node([self.node_a])
-    #     assert neighbor.get_neighbors()[0] is self.node_a, "Neighbor not added"
-    #     assert self.node_a.get_neighbors() == [], "Neighbor list should not be updated automatically @Directed"
-    #     #todo pass multiple
-    #     #todo pass Node instead of list
-    #
-    # def test_add_neighbors_individual(self):
-    #     neighbor = Node()
-    #     neighbor.add_neighbor(self.node_a)
-    #     assert neighbor.get_neighbors()[0] is self.node_a, "Neighbor not added"
-    #     assert self.node_a.get_neighbors() == [], "Neighbor list should not be updated automatically @Directed"
-    #
-    #     #todo add fail cases
-    #
-    # def test_add_neighbors_starlist(self):
-    #     neighbor_a = Node()
-    #     neighbor_a.add_neighbor(*[self.node_a])
-    #     assert neighbor_a.get_neighbors()[0] is self.node_a, "Neighbor not added"
-    #     assert self.node_a.get_neighbors() == [], "Neighbor list should not be updated automatically @Directed"
-    #
-    #
-    #     #todo pass list
-    #     #todo pass tuple
-    #     #todo add fail cases
-    #     #todo add neighbors with other neighbors
-
-
-if __name__ == "__main__":
-    unittest.main()     # run all tests
