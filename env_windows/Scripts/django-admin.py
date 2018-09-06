4865764443c113c9e83bd4dc2b75be7ec4de23fa
Yohan Francis <yohan@keaz.co>
2018-09-06 23:08:46 +1000

    windows environment

---
+++ env_windows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yohan\documents\git\3162_sna_g17\.env_windows\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
