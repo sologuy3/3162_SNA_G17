01c84c7c5f25f714fdd1ca592a20fd2b5301816a
Yohan <yohan@keaz.co>
2018-09-06 15:12:59 +0200

    fix namee

--- env_windows/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\yohan\documents\git\3162_sna_g17\.env_windows\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
