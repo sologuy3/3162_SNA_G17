394de2a368bfce0478d9fae00a9b65ead6c9b497
Yohan Francis <yohan@keaz.co>
2018-10-24 16:55:06 +1100

    fix linux startup

--- run_linux.sh
+++ run_linux.sh
@@ -1,8 +1,4 @@
-rm -rf .env
-mkdir .env
-python3 -m venv .env
 source .env/bin/activate
-pip install -r requirements.txt
 cd sna
 python manage.py runserver &
 google-chrome-stable 127.0.0.1:8000/vis
